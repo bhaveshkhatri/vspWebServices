80b750e06578677b4d116402fea1744b432e9c36
bhaveshkhatri <bhavesh.khatri@gmail.com>
2016-12-01 17:10:03 -0600

    Added skeleton post processor plugins at the WebTest and LoadTest level.

---
+++ VspWS/VspWS.Plugins/LoadTest/LoadTestPostProcessor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.LoadTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VspWS.Plugins.LoadTest
+{
+    public class LoadTestPostProcessor : ILoadTestPlugin
+    {
+        Microsoft.VisualStudio.TestTools.LoadTesting.LoadTest myLoadTest;
+
+        public void Initialize(Microsoft.VisualStudio.TestTools.LoadTesting.LoadTest loadTest)
+        {
+            myLoadTest = loadTest;
+            myLoadTest.TestFinished += new EventHandler<TestFinishedEventArgs>(myLoadTest_LoadTestFinished);
+        }
+
+        void myLoadTest_LoadTestFinished(object sender, TestFinishedEventArgs e)
+        {
+            // TODO
+        }
+    }
+}
---
+++ VspWS/VspWS.Plugins/WebTest/WebTestPostProcessor.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.WebTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace VspWS.Plugins.WebTest
+{
+    public class WebTestPostProcessor : WebTestPlugin
+    {
+        private List<int> _responseCodes = new List<int>();
+
+        public override void PostWebTest(object sender, PostWebTestEventArgs e)
+        {
+            base.PostWebTest(sender, e);
+
+            if (_responseCodes.Any(x => x != (int)HttpStatusCode.OK))
+            {
+                e.WebTest.Outcome = Outcome.Fail;
+                e.WebTest.AddCommentToResult("Not all responses had status code 200.");
+            }
+
+            //Debugger.Launch();
+        }
+
+        public override void PostRequest(object sender, PostRequestEventArgs e)
+        {
+            base.PostRequest(sender, e);
+
+            this._responseCodes.Add((int)e.Response.StatusCode);
+
+            //Debugger.Launch();
+        }
+    }
+}
