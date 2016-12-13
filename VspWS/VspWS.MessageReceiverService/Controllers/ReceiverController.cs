370c2e5b25cc6adacb6735ca6b6d9098066219ef
bhaveshkhatri <bhavesh.khatri@gmail.com>
2016-12-13 13:59:23 -0600

    Remove conflicting endpoint.

--- VspWS/VspWS.MessageReceiverService/Controllers/ReceiverController.cs
+++ VspWS/VspWS.MessageReceiverService/Controllers/ReceiverController.cs
@@ -32,8 +32,8 @@ namespace VspWS.MessageReceiverService.Controllers
             return Post(messages.FirstOrDefault());
         }
 
-        [HttpPost]
-        public IHttpActionResult Post(Message message)
+        //[HttpPost]
+        private IHttpActionResult Post(Message message)
         {
             try
             {
